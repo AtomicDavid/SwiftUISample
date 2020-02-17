7f7a28d6ae83ef2f6d0c69b67eff7c3fb0b0c91e
David Atkinson <david@atomicmedia.co.uk>
2020-02-17 16:33:41 +0000

    Hello World

---
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by David Atkinson on 17/02/2020.
+//  Copyright © 2020 Crest. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
